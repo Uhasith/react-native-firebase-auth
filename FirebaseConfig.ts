68e66e1d344bbadbce5b153cf36e73d109c408ac
Hasith Udayanga <78094049+Uhasith@users.noreply.github.com>
2024-09-27 13:16:09 +0530

    Create FirebaseConfig.ts

---
+++ FirebaseConfig.ts
@@ -0,0 +1,24 @@
+// FirebaseConfig.ts
+import { initializeApp } from "firebase/app";
+import { initializeAuth, getReactNativePersistence } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import ReactNativeAsyncStorage from "@react-native-async-storage/async-storage";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+const auth = initializeAuth(app, {
+  persistence: getReactNativePersistence(ReactNativeAsyncStorage),
+});
+
+export { app, auth, db };
