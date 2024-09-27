66101022a7ab30284bda378c4010ac948269dc7b
Hasith Udayanga <78094049+Uhasith@users.noreply.github.com>
2024-09-27 13:09:33 +0530

    Update FirebaseConfig.ts

--- FirebaseConfig.ts
+++ FirebaseConfig.ts
@@ -6,12 +6,12 @@ import ReactNativeAsyncStorage from "@react-native-async-storage/async-storage";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBqNMEkThGopg6OWKyGOF71fhuDDDxE0PI",
-  authDomain: "doggy-food-b1252.firebaseapp.com",
-  projectId: "doggy-food-b1252",
-  storageBucket: "doggy-food-b1252.appspot.com",
-  messagingSenderId: "551856074814",
-  appId: "1:551856074814:web:8edc72a94c31600c24a16f",
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
 };
 
 // Initialize Firebase
